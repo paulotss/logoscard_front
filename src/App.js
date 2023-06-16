c426cde309bd4a32ee7e5489a44a2faa9866823c
Paulo de Tarso <paulo.oinab@gmail.com>
2023-06-16 15:05:13 -0300

    create ClientsPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import ClientsPage from './pages/ClientsPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/clients' element={<ClientsPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/ClientsPage.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+
+const ClientsPage = () => {
+  return (
+    <>
+      <Header />
+      <main className="p-5">
+        <p className="font-bold mb-3">Clientes</p>
+      </main>
+    </>
+  )
+}
+
+export default ClientsPage;
