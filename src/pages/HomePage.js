fa1a77c92623c5c2578754e9948f59c97965f56c
Paulo de Tarso <paulo.oinab@gmail.com>
2023-06-16 13:21:32 -0300

    create ButtonsSection component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,9 @@
+import ButtonSection from "../components/ButtonSection";
 import Header from "../components/Header";
 import Invoices from "../components/Invoices";
+import { HiUserGroup } from 'react-icons/hi2';
+import { HiDocumentText } from 'react-icons/hi2';
+import { HiRectangleGroup } from 'react-icons/hi2';
 
 const HomePage = () => {
   return (
@@ -8,6 +12,23 @@ const HomePage = () => {
       <main className="p-5">
         <p className="font-bold mb-3">Faturas</p>
         <Invoices />
+        <section className="flex justify-around p-5 mt-5 border-t-2 border-gray-400">
+          <ButtonSection
+            title="Clientes"
+            image={ <HiUserGroup /> }
+            path="/"
+          />
+          <ButtonSection
+            title="Faturas"
+            image={ <HiDocumentText /> }
+            path="/"
+          />
+          <ButtonSection
+            title="Planos"
+            image={ <HiRectangleGroup /> }
+            path="/"
+          />
+        </section>
       </main>
     </>
   )
