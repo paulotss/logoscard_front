f262a09cb9ec6a4dded6c3c387b60a1c2252525c
Paulo de Tarso <paulo.oinab@gmail.com>
2023-06-16 16:20:06 -0300

    create grid for receive data

--- src/pages/ClientsPage.js
+++ src/pages/ClientsPage.js
@@ -6,6 +6,18 @@ const ClientsPage = () => {
       <Header />
       <main className="p-5">
         <p className="font-bold mb-3">Clientes</p>
+        <div className="w-full">
+          <div className="grid grid-gap grid-cols-4 grid-rows-1  p-2">
+            <div>Inscrição</div>
+            <div className="col-span-2">Nome</div>
+            <div className="text-center">Status</div>
+          </div>
+          <div className="grid grid-gap grid-cols-4 grid-rows-1 bg-gray-400 rounded-lg p-2">
+            <div>200568</div>
+            <div className="col-span-2">Paulo de Tarso Soares Silva</div>
+            <div className="text-center">ok</div>
+          </div>
+        </div>
       </main>
     </>
   )
