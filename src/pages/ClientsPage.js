5f08c150ab439d789eb62a3f16365d95536b7a63
Paulo de Tarso <paulo.oinab@gmail.com>
2023-06-16 17:35:57 -0300

    fetch clients data

--- src/pages/ClientsPage.js
+++ src/pages/ClientsPage.js
@@ -1,22 +1,48 @@
+import { useState, useEffect } from 'react';
 import Header from "../components/Header";
+import axios from "../http";
+import loading from "../media/isLoading.gif";
 
 const ClientsPage = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  useEffect(() => {
+    const getUsers = async () => {
+      setIsLoading(true);
+      const result = await axios.get('/users');
+      setUsers(result.data);
+      setIsLoading(false);
+    }
+    getUsers();
+  }, [])
   return (
     <>
       <Header />
       <main className="p-5">
         <p className="font-bold mb-3">Clientes</p>
         <div className="w-full">
-          <div className="grid grid-gap grid-cols-4 grid-rows-1  p-2">
+          <div className="grid grid-gap grid-cols-4 grid-rows-1 p-2 text-sm">
             <div>Inscrição</div>
             <div className="col-span-2">Nome</div>
-            <div className="text-center">Status</div>
+            <div className="text-right">Status</div>
           </div>
-          <div className="grid grid-gap grid-cols-4 grid-rows-1 bg-gray-400 rounded-lg p-2">
-            <div>200568</div>
-            <div className="col-span-2">Paulo de Tarso Soares Silva</div>
-            <div className="text-center">ok</div>
-          </div>
+          {
+            isLoading
+              ? <div className="flex justify-center w-full">
+                  <img src={ loading } alt="" />
+                </div>
+              : users.map((user) => (
+                  <div
+                    key={ user.id }
+                    className="grid grid-gap grid-cols-4 grid-rows-1 bg-gray-400 rounded-lg p-2 mb-2"
+                  >
+                    <div>{ user.id }</div>
+                    <div className="col-span-2">{ `${user.firstName} ${user.lastName}` }</div>
+                    <div className="text-right pr-2">ok</div>
+                  </div>
+                ))
+          }
         </div>
       </main>
     </>
