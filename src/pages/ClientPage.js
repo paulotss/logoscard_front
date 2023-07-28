945eb337d173810cc4b56a7a070ce9342907e566
Paulo de Tarso <paulo.oinab@gmail.com>
2023-07-28 16:40:45 -0300

    fix show birthday in client and dependent page

--- src/pages/ClientPage.js
+++ src/pages/ClientPage.js
@@ -28,7 +28,10 @@ const ClientPage = () => {
 
   const formatDate = (value) => {
     const date = new Date(value);
-    return `${date.getDay()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+    const month = (date.getMonth() + 1) < 10 ? `0${date.getMonth() + 1}` : (date.getMonth() + 1);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
   }
 
   useEffect(() => {
