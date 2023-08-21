2013b3ab4246700083ffcbf3fa8b7dfecf8e5ace
Paulo de Tarso <paulo.oinab@gmail.com>
2023-08-21 11:03:52 -0300

    add top-menu in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,15 +10,41 @@ const Header = () => {
 
   return (
     <header
-    className="flex justify-between bg-gray-900 text-white p-5"
+    className="flex justify-between bg-gray-900 text-white p-5 items-center"
     >
-      <Link to="/" className="bg-gray-300 p-2 rounded-full font-bold text-gray-900">
+      <Link to="/" className="bg-gray-300 p-2 h-fit rounded-full font-bold text-gray-900">
         LOGOSCARD
       </Link>
+      <nav className="flex list-none w-full p-2 justify-center">
+        <Link
+          to="/"
+          className="p-2 hover:bg-gray-300 hover:text-gray-900 rounded-md"
+        >
+          Home
+        </Link>
+        <Link
+          to="/clients"
+          className="p-2 hover:bg-gray-300 hover:text-gray-900 rounded-md"
+        >
+          Clientes
+        </Link>
+        <Link
+          to="/dependents"
+          className="p-2 hover:bg-gray-300 hover:text-gray-900 rounded-md"
+        >
+          Dependentes
+        </Link>
+        <Link
+          to="/cashflow"
+          className="p-2 hover:bg-gray-300 hover:text-gray-900 rounded-md"
+        >
+          Caixa
+        </Link>
+      </nav>
       <div className="flex">
         <button
           onClick={logout}
-          className="bg-gray-300 p-2 rounded-full font-bold text-gray-900 w-20 text-center"
+          className="bg-gray-300 p-2 h-fit rounded-full font-bold text-gray-900 w-20 text-center"
         >
           Sair
         </button>
