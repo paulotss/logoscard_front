d0ba8fb606b07086c1e946522e51a87a74026675
Paulo de Tarso <paulo.oinab@gmail.com>
2023-08-22 16:01:43 -0300

    add search bar for clients

--- src/pages/ClientsListPage.js
+++ src/pages/ClientsListPage.js
@@ -6,7 +6,29 @@ import { Link } from 'react-router-dom';
 
 const ClientsListPage = () => {
   const [clients, setClients] = useState([]);
+  const [filterClients, setFilterClients] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const extractNameDependents = (dependents) => {
+    const result = dependents.reduce((acc, dependent) => (
+      acc += `${dependent.user.firstName} ${dependent.user.lastName}`.toLowerCase()
+    ), "");
+    return result;
+  }
+
+  const handleChangeSearchBar = ({ target }) => {
+    const { value } = target;
+    const result = clients.filter((client) => {
+      const { firstName, lastName } = client.user;
+      const fullNameClient = `${firstName} ${lastName}`.toLowerCase();
+      const fullNamesDependents = extractNameDependents(client.user.assignment.dependents);
+      const term = value.toLowerCase();
+      return fullNameClient.includes(term) || fullNamesDependents.includes(term);
+    });
+    setSearchTerm(target.value);
+    setFilterClients(result);
+  }
 
   const checkLateInvoice = (invoices) => {
     const today = new Date();
@@ -25,8 +47,8 @@ const ClientsListPage = () => {
     const getUsers = async () => {
       setIsLoading(true);
       const result = await axios.get('/clients');
-      console.log(result.data);
       setClients(result.data);
+      setFilterClients(result.data);
       setIsLoading(false);
     }
     getUsers();
@@ -36,6 +58,14 @@ const ClientsListPage = () => {
       <Header />
       <main className="p-5">
         <p className="font-bold mb-3">Clientes</p>
+        <div className="mb-2">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleChangeSearchBar}
+            className="w-full p-2 rounded-md"
+          />
+        </div>
         <div className="w-full">
           <div className="grid grid-gap grid-cols-4 grid-rows-1 p-2 text-sm">
             <div>Inscrição</div>
@@ -47,24 +77,38 @@ const ClientsListPage = () => {
               ? <div className="flex justify-center w-full">
                   <img src={ loading } alt="" />
                 </div>
-              : clients.map((client) => (
-                  <Link to={`/client/${client.user.id}`}
-                    key={ client.id }
-                    className="grid grid-gap grid-cols-4 grid-rows-1 bg-gray-400 rounded-lg p-2 mb-2"
-                  >
-                    <div>{ client.id }</div>
-                    <div className="col-span-2">{ `${client.user.firstName} ${client.user.lastName}` }</div>
-                    <div className="text-right pr-2">
-                      { !client.user.assignment
-                        ? <div className="w-3 h-3 bg-slate-700 rounded-full inline-block"> </div> 
-                        : checkLateInvoice(client.user.invoices)
-                          ? <div className="w-3 h-3 bg-red-700 rounded-full inline-block"> </div>
-                          : checkPendingInvoice(client.user.invoices)
-                            ? <div className="w-3 h-3 bg-amber-500 rounded-full inline-block"> </div>
-                            : <div className="w-3 h-3 bg-green-700 rounded-full inline-block"> </div>
-                      }
-                    </div>
-                  </Link>
+              : filterClients.map((client) => (
+                  <>
+                    <Link to={`/client/${client.user.id}`}
+                      key={ client.id }
+                      className="grid grid-gap grid-cols-4 grid-rows-1 bg-gray-400 rounded-lg p-2 mt-2"
+                    >
+                      <div>{ client.id }</div>
+                      <div className="col-span-2">{ `${client.user.firstName} ${client.user.lastName}` }</div>
+                      <div className="text-right pr-2">
+                        { !client.user.assignment
+                          ? <div className="w-3 h-3 bg-slate-700 rounded-full inline-block"> </div> 
+                          : checkLateInvoice(client.user.invoices)
+                            ? <div className="w-3 h-3 bg-red-700 rounded-full inline-block"> </div>
+                            : checkPendingInvoice(client.user.invoices)
+                              ? <div className="w-3 h-3 bg-amber-500 rounded-full inline-block"> </div>
+                              : <div className="w-3 h-3 bg-green-700 rounded-full inline-block"> </div>
+                        }
+                      </div>
+                    </Link>
+                    {
+                      client.user.assignment.dependents.map((dependent) => (
+                        <Link to={`/dependent/${dependent.id}`}
+                          key={ dependent.id }
+                          className="grid grid-gap grid-cols-4 grid-rows-1 bg-green-200 p-2 mb-2"
+                        >
+                          <div>{ dependent.id }</div>
+                          <div className="col-span-2">{ `${dependent.user.firstName} ${dependent.user.lastName}` }</div>
+                          <div> </div>
+                        </Link>
+                      ))
+                    }
+                  </>
                 ))
           }
         </div>
