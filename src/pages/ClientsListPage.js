08c94240b2bc131526db7a9aaa6ebd1e8c8ded30
Paulo de Tarso <paulo.oinab@gmail.com>
2023-08-29 17:46:11 -0300

    fix dependents show in clients list page

--- src/pages/ClientsListPage.js
+++ src/pages/ClientsListPage.js
@@ -97,11 +97,11 @@ const ClientsListPage = () => {
                       </div>
                     </Link>
                     {
-                      client.user.assignment &&
-                      client.user.assignment.dependents.map((dependent) => (
+                      client.user.assignments.length > 0 &&
+                      client.user.assignments[0].dependents.map((dependent) => (
                         <Link to={`/dependent/${dependent.id}`}
                           key={ dependent.id }
-                          className="grid grid-gap grid-cols-4 grid-rows-1 bg-green-200 p-2 mb-2"
+                          className="grid grid-gap grid-cols-4 grid-rows-1 bg-[#6DBDB7] p-2 mb-2"
                         >
                           <div>{ dependent.id }</div>
                           <div className="col-span-2">{ `${dependent.user.firstName} ${dependent.user.lastName}` }</div>
