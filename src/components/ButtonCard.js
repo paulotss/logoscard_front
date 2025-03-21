9f3dae9ee568d474e4e0f527bcc8525c22d20dfd
AlexandreFB2003 <afborges2003@gmail.com>
2025-03-20 21:22:05 -0300

    input valor assinatura

--- src/components/ButtonCard.js
+++ src/components/ButtonCard.js
@@ -2,17 +2,33 @@ import { useState } from "react";
 
 const ButtonCard = ({ id }) => {
     const [link, setLink] = useState("");
+    const [amount, setAmount] = useState("");
   
     const generateLink = () => {
       const newLink = `${window.location.origin}/card/create/${id}`;
       setLink(newLink);
     };
 
+    const handleAmountChange = (event) => {
+      const value = event.target.value.replace(/\D/g, ""); // Permite apenas números
+      setAmount(value);
+    };
+
   return (
     <div className="flex flex-col items-start w-80"> 
+    <label htmlFor="amount" className="mb-2">Digite o valor da assinatura:</label>
+        <input
+        id="amount"
+        type="text"
+        value={amount}
+        onChange={handleAmountChange}
+        className="w-full p-2 border border-black rounded-md mb-3"
+        placeholder="Digite um valor"
+        />
       <button
         onClick={generateLink}
-        className="p-2 w-24 bg-green-900 text-white rounded-md"  
+        className={`p-2 w-24 bg-green-900 text-white rounded-md ${!amount ? 'opacity-50 cursor-not-allowed' : ''}`}
+        disabled={!amount}  
       >
         Gerar
       </button>
